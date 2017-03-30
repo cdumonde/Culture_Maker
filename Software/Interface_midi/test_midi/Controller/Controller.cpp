dad5f613627beabaa887d8929c8bde20b072ffe4
Clovel <cd.clovel19@gmail.com>
2017-03-30 13:21:33 +0200

    Update Controller
    Suite du code. Début des codes sur Save et Load dans les .mod

--- Software/Interface_midi/test_midi/Controller/Controller.cpp
+++ Software/Interface_midi/test_midi/Controller/Controller.cpp
@@ -1,10 +1,12 @@
+// Clovis Durand
+
 #include "Controller.h"
 using namespace std;
 
-// Con/Destructeurs
+// Con/Destructeurs -------------------------------------------------
 Controller::Controller(unsigned char id_physical, 
 				 unsigned char id_function, 
-				 ControllerER_TYPE type)
+				 CONTROLLER_TYPE type)
 {
 	// Contructeur
 	m_id_phys(id_physical);
@@ -12,9 +14,9 @@ Controller::Controller(unsigned char id_physical,
 	m_type(type);
 }
 
-Controller::Controller(string const filename)
+Controller::Controller(unsigned char const id)
 {
-	loadController(filename);
+	loadController(id);
 }
 
 Controller::~Controller()
@@ -22,7 +24,14 @@ Controller::~Controller()
 	// Destructeur
 }
 
-// Accesseurs
+// Afficheurs -------------------------------------------------------
+string infosController(void) const
+{
+	return to_string(get_id_phys()) + "/" + to_string(get_id_funct()) + "/" + type_to_string(get_type());
+}
+void print_Controller(void) const
+
+// Accesseurs -------------------------------------------------------
 unsigned char Controller::get_id_phys(void) const
 {
 	return m_id_phys;
@@ -33,7 +42,7 @@ unsigned char Controller::get_id_funct(void) const
 	return m_id_funct;
 }
 
-ControllerER_TYPE Controller::get_type(void) const
+CONTROLLER_TYPE Controller::get_type(void) const
 {
 	return m_type;
 }
@@ -48,7 +57,7 @@ unsigned char Controller::get_value(void) const
 	return m_value;
 }
 
-// Mutateurs
+// Mutateurs ---------------------------------------------------------
 void Controller::set_id_phys(unsigned char const &id)
 {
 	m_id_phys = id;
@@ -58,7 +67,7 @@ void Controller::set_id_funct(unsigned char const &id)
 	m_id_funct = id;
 }
 
-void Controller::set_type(ControllerER_TYPE const &type)
+void Controller::set_type(CONTROLLER_TYPE const &type)
 {
 	m_type = type;
 }
@@ -71,4 +80,80 @@ void Controller::set_velocity(unsigned char const &velocity)
 void Controller::set_value(unsigned char const &value)
 {
 	m_value = value;
+}
+
+// File Management --------------------------------------------------
+bool Controller::loadController(unsigned char const &id)
+{
+	string filename = to_string(id) + ".mod"
+	return loadController(filename);
+}
+
+bool Controller::loadController(string const filename)
+{
+	ifstream flux(filename)
+
+	if(flux)
+	{
+		string ligne;
+		getline(flux, ligne);
+		flux.close();
+
+		string infos[3];
+		unsigned int pos, taille;
+
+		for (int i = 0; i < 3; i++)
+		{
+			taille = ligne.size();
+			if (taille != 0)
+			{
+				pos  = ligne.find("/");
+				infos[i] = ligne.substr(0, pos);
+				ligne    = ligne.substr(pos + 1, taille - 1);
+			}
+		}
+
+		m_id_phys  = infos[0];
+		m_id_funct = infos[1];
+		m_type	   = string_to_type(infos[2]);
+
+		cout << "LOADED : " << *this << " from " << filename << endl;
+	}
+	else
+	{
+		cerr << "ERROR  : Cannot open file [" << filename << "] (read mode)." << endl;
+		return 1;
+	}
+	return 0;
+}
+
+bool Controller::saveController(void) const
+{
+	string filename = to_string(id) + ".mod";
+	ofstream flux(filename.c_str(), ios::app); // ofstream o comme output
+	//Déclaration d'un flux permettant d'écrire dans un fichier.
+	//ios::app = seek the end of file before you write
+
+	string infos = infosController();
+
+	if(flux)
+	{
+		flux << infos;
+		flux.close();
+		cout << "WROTE  : " << infos << " in " << filename << endl;
+
+		return 0;
+	}
+	else
+	{
+		cerr << "ERROR  : Cannot open file [" << filename << "] (write mode)." << endl;
+		return 1;
+	}
+}
+
+// Operateurs -------------------------------------------------------
+ostream &operator<<(ostream &flux, Controller const &c)
+{
+	flux << c.infosController();
+	return flux;
 }
--- Software/Interface_midi/test_midi/Controller/Controller.h
+++ Software/Interface_midi/test_midi/Controller/Controller.h
@@ -1,42 +1,50 @@
+// Clovis Durand
+
 #ifndef Controller_H
 #define Controller_H
 
-enum ControllerER_TYPE
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+enum CONTROLLER_TYPE
 {
 	// Type de module
 	BUTTON,
 	SLIDER,
-	PIEZO,
-	POTARD,
-	CAPT_IR,
-	ENCODER, 
-	UNKNOWN
+	PIEZO,			// Piezoelectric button
+	POTENTIOMETER,
+	IR_SENSOR,
+	ENCODER, 		// wtf ?!?
+	UNKNOWN 		// Default type
 };
 
 class Controller
 {
 public:
 	// Con/Destructeurs
-	Controller(unsigned char const id_physical, 
-			unsigned char const id_function, 
-			ControllerER_TYPE type = UNKNOWN);
-	Controller(std::string const filename);
+	Controller(unsigned char const id_physical = 0, 
+			unsigned char const id_function    = 0, 
+			CONTROLLER_TYPE type               = UNKNOWN);
+	Controller(unsigned char const id);
 	~Controller();
 
 	// Afficheurs
+	std::string infosController(void) const; 
 	void print_Controller(void) const;
 
 	// Accesseurs
 	unsigned char get_id_phys(void) const;
 	unsigned char get_id_funct(void) const;
-	ControllerER_TYPE get_type(void) const;
+	CONTROLLER_TYPE get_type(void) const;
 	unsigned char Controller::get_velocity(void) const;
 	unsigned char Controller::get_value(void) const;
 
 	// Mutateurs
 	void set_id_phys(unsigned char const &id);
 	void set_id_funct(unsigned char const &id);
-	void set_type(ControllerER_TYPE const &type);
+	void set_type(CONTROLLER_TYPE const &type);
 	void set_velocity(unsigned char const &velocity);
 	void set_value(unsigned char const &value);
 
@@ -45,16 +53,20 @@ public:
 	// Méthodes publiques diverses
 
 	// File management
-	void loadController(std::string const filename);
-	void saveController(std::string const filename);
+	bool loadController(unsigned char const &id);
+	bool loadController(std::string const filename);
+	bool saveController(void) const;
 
 private:
 	// Attributs
 	unsigned char m_id_phys, m_id_funct; 	// IDs du module concerné
-	ControllerER_TYPE m_type; 					// Type de module concerné
+	CONTROLLER_TYPE m_type; 					// Type de module concerné
 	unsigned char m_velocity, m_value;		// Action sur le module concerné
 
 	// Méthodes privées diverses
+	std::string type_to_string(void) const;
+	CONTROLLER_TYPE string_to_type(std::string strtype) const;
+	unsigned char type_to_uChar(void) const;
 };
 
 // Operateurs
